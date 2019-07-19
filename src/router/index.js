10efeb4aa5f924f1aa15f9be868c93c07488230c
thunderfreeze <yoyot32800@gmail.com>
2019-07-19 13:00:45 +0200

    :sparkle:add body class on each pages

--- src/router/index.js
+++ src/router/index.js
@@ -7,30 +7,57 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import("@/pages")
+    component: () => import("@/pages"),
+    meta: {
+      bodyClass: "home"
+    }
   },
   {
     path: "/a-propos",
     name: "About",
-    component: () => import("@/pages/about")
+    component: () => import("@/pages/about"),
+    meta: {
+      bodyClass: "about"
+    }
   },
   {
     path: "/horaire",
     name: "Schedule",
-    component: () => import("@/pages/schedule")
+    component: () => import("@/pages/schedule"),
+    meta: {
+      bodyClass: "schedule"
+    }
   },
   {
     path: "/question",
     name: "Question",
-    component: () => import("@/pages/question")
+    component: () => import("@/pages/question"),
+    meta: {
+      bodyClass: "question"
+    }
   },
   {
     path: "/reponse",
     name: "Answer",
-    component: () => import("@/pages/answer")
+    component: () => import("@/pages/answer"),
+    meta: {
+      bodyClass: "answer"
+    }
   }
 ];
 
-export default new Router({
+let router = new Router({
   routes // short for `routes: routes`,
 });
+
+router.beforeEach((to, from, next) => {
+  document.body.classList.add(to.meta.bodyClass);
+  document.body.classList.remove(from.meta.bodyClass);
+
+  console.log("from", from.meta.bodyClass);
+  console.log("to", to.meta.bodyClass);
+
+  next();
+});
+
+export default router;
