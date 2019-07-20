b7c7690ed1dd10c32512b0e6b2eb7e186e510416
thunderfreeze <yoyot32800@gmail.com>
2019-07-21 00:15:44 +0200

    :sparkle: add fantome picture and text

--- src/pages/answer.vue
+++ src/pages/answer.vue
@@ -4,6 +4,10 @@
     h2.page 01/10
     p.bonne-reponse La bonne réponse était
     h1.fantome LE FANTOME DE L'OPERA
+    img.picture(src="static/images/fantome.jpg")
+    p.diff.diffusion Diffusé a 20:00
+    p.diffusion Le vendredi 7 juin 2019
+    p.diffusion Au café-cinéma Videodrome-2 à Marseille   
 </template>
 
 <style lang="sass">
@@ -15,6 +19,13 @@
   font-size: 40px
   font-family: "Bloodlust"
 
+.diff
+  padding-top: 50px
+
+.diffusion
+  font-family: "Montserrat-Medium"
+  font-size: 25px
+
 .page
   position: absolute
   right: 42px
@@ -25,6 +36,11 @@
 
 .bonne-reponse
   padding-top: 62px
+  font-family: "Montserrat-Medium"
+
+.fantome
+  font-family: "Montserrat-Bold"
+  color: #F1C53E
 </style>
 <script>
 export default {
