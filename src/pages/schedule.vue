1166b198db8e165802a134128582c744da9c6486
thunderfreeze <yoyot32800@gmail.com>
2019-07-20 16:37:11 +0200

    :sparkle: Add title on the schedule page, and put some styles on

--- src/pages/schedule.vue
+++ src/pages/schedule.vue
@@ -2,7 +2,8 @@
   section
     div.schedule-size
       h2.mechant QUESTION POUR UN MECHANT
-      h3.prog PROGRAMMATION
+      h3.prog BRAVO TU AS TERMINE LE QUIZZ
+      h3.prog.programation PROGRAMMATION
       p.horaire.date Vendredi 7 juin 2019
       p.horaire 20:00  Le fantôme de l'opéra (Rupert Julian) 1925
       p.horaire 22:00  L'homme qui rit (Jonathan Demme) 1928
@@ -15,7 +16,7 @@
       p.horaire 15:30  Le chat noir (Edgar G. Ulmer) 1934
       p.horaire 17:30  Le corbeau (Lew Landers) 1935
       p.horaire 20:00  Le loup garou (M.?ight Shyamalan)1941
-      p 22:00  Psychose (Alphred Hitchcock)1960
+      p.horaire 22:00  Psychose (Alphred Hitchcock)1960
       p.adresse.pad AU CINEMA-CAFE VIDEODROME-2
       p.adresse 49 Cours Julien - 13006 Marseille
       p.adresse 04 91 42 75 41
@@ -31,27 +32,39 @@
   margin-left: 25%
 
 .prog
-  padding-top: 80px
-  padding-bottom: 0px
+  padding-top: 0
+  padding-bottom: 0
   margin-bottom: 0
+  font-size: 24px
+  font-family: "Montserrat-Bold"
+
+.programation
+  padding-top: 12px
 
 .mechant
-  padding-top: 50px
+  padding-top: 95px
+  padding-bottom: 26px
+  font-family: "Bloodlust"
+  font-size: 70px
 
 .horaire
   text-align: left
   width: 100%
   padding-left: 25%
+  font-family: "Montserrat-Medium"
+  font-size: 18px
 
 .date
   padding-top: 40px
-  font-weight: 900
+  font-family: "Montserrat-Bold"
+  font-size: 22px
 
 .pad
   padding-top: 50px
 
 .adresse
-  font-weight: 900
+  font-family: "Montserrat-Bold"
+  font-size: 25px
 </style>
 
 <script>
