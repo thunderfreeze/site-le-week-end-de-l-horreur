47f9e0a24c67747bf1499f34846ed38e421ba73f
thunderfreeze <yoyot32800@gmail.com>
2019-07-20 17:00:46 +0200

    :sparkle: Copy paste element from "question.vue" to "answer.vue"  (.question-pour-un-mechant and .page)

--- src/pages/answer.vue
+++ src/pages/answer.vue
@@ -3,8 +3,25 @@
     h1 Answer
 </template>
 
+<style lang="sass">
+.question-pour-un-mechant
+  position: absolute
+  left: 34px
+  top: 34px
+  color: white
+  font-size: 40px
+  font-family: "Bloodlust"
+
+.page
+  position: absolute
+  right: 42px
+  top: 34px
+  color: white
+  font-size: 40px
+  font-family: "Bloodlust"
+</style>
 <script>
 export default {
-	name: "Answer"
+  name: "Answer"
 };
 </script>
