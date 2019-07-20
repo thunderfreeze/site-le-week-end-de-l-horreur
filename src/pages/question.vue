fb353bd039587eae4ddca1111e398e8b8ae85086
thunderfreeze <yoyot32800@gmail.com>
2019-07-20 16:58:51 +0200

    :sparkle: Add new styles on "question.vue"

--- src/pages/question.vue
+++ src/pages/question.vue
@@ -3,9 +3,9 @@
     h2.question-pour-un-mechant QUESTION POUR UN MECHANT
     h2.page 01/10
     h1.question-title QUI DORT DANS UN OPERA POUR PECHO?
-    p Les rats d'Opéra
-    p Le fantôme de l'Opéra
-    p La danseuse étoilée
+    p.reponse Les rats d'Opéra
+    p.reponse Le fantôme de l'Opéra
+    p.reponse La danseuse étoilée
 </template>
 
 <style lang="sass">
@@ -28,10 +28,19 @@ body
   font-size: 40px
   font-family: "Bloodlust"
 
+.reponse
+  font-size: 30px
+  font-family: "Montserrat-Bold"
+  padding-bottom: 20px
+  color: #E9D4AE
+
 .question-title
   text-align: center
-  padding-top: 350px
-  color: #F0CF2C
+  padding-top: 405px
+  padding-bottom: 50px
+  font-size: 70px
+  color: #F1C53E
+  font-family: "AvenirNextCondensed-HeavyItalic-10"
 </style>
 
 <script>
