1563f564502c6b557c5a29017e7ef44c710e3db4
thunderfreeze <yoyot32800@gmail.com>
2019-07-22 15:26:02 +0200

    :sparkles: Import store from "@/store"

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 
 import App from "@/app.vue";
 import router from "@/router";
+import store from "@/store";
 
 // See: https://vuejs.org/v2/api/#productionTip
 Vue.config.productionTip = false;
@@ -9,6 +10,7 @@ Vue.config.productionTip = false;
 const app = new Vue({
   el: "#app",
   router,
+  store,
   components: { App },
   template: "<App/>"
 });
