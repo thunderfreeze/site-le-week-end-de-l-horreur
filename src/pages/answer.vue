2c9213cc2049650843e878280ed6e0f93cd813f6
thunderfreeze <yoyot32800@gmail.com>
2019-07-22 12:25:42 +0200

    :sparkles: Add background color for each answer and add props: ["answer_number"]

--- src/pages/answer.vue
+++ src/pages/answer.vue
@@ -3,14 +3,50 @@
     h2.question-pour-un-mechant QUESTION POUR UN MECHANT
     h2.page 01/10
     p.bonne-reponse La bonne réponse était
-    h1.fantome LE FANTOME DE L'OPERA
-    img.picture(src="static/images/fantome.jpg")
-    p.diff.diffusion Diffusé a 20:00
-    p.diffusion Le vendredi 7 juin 2019
-    p.diffusion Au café-cinéma Videodrome-2 à Marseille   
+
+    h1.fantome(v-if="answer_number == 1") LE FANTOME DE L'OPERA
+
+    img.picture(v-if="answer_number == 1")(src="static/images/fantome.jpg")
+
+    p.diff.diffusion(v-if="answer_number == 1") Diffusé a 20:00
+    p.diffusion(v-if="answer_number == 1") Le vendredi 7 juin 2019
+    p.diffusion(v-if="answer_number == 1")answer Au café-cinéma Videodrome-2 à Marseille   
 </template>
 
 <style lang="sass">
+
+//<BACKGROUND>//
+
+.answer-1
+  background-color: #BB3537
+
+.answer-2
+  background-color: #387078
+
+.answer-3
+  background-color: #2A4031
+
+.answer-4
+  background-color: #323639
+
+.answer-5
+  background-color: #241B2A
+
+.answer-6
+  background-color: #010244
+
+.answer-7
+  background-color: #131313
+
+.answer-8
+  background-color: #382532
+
+.answer-9
+  background-color: #2A397A
+
+.answer-10
+  background-color: #000000
+//</BACKGROUND>//
 .question-pour-un-mechant
   position: absolute
   left: 34px
@@ -44,6 +80,7 @@
 </style>
 <script>
 export default {
-  name: "Answer"
+  name: "Answer",
+  props: ["answer_number"]
 };
 </script>
