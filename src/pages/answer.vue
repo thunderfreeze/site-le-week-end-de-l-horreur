5c6c9dc53f5858cd9da7153426b74d5016475055
thunderfreeze <yoyot32800@gmail.com>
2019-07-23 12:28:23 +0200

    :wrench: rename answer-title and reponse-title to question-answer-title and add the cracked effect on title

--- src/pages/answer.vue
+++ src/pages/answer.vue
@@ -13,25 +13,25 @@
     h2.page(v-if="answer_number == 10") 10/10
     p.bonne-reponse La bonne réponse était
 
-    h1.answer-title-1.answer-title(v-if="answer_number == 1") LE FANTOME DE L'OPERA
+    h1.question-answer-title-1.question-answer-title(v-if="answer_number == 1") LE FANTOME DE L'OPERA
 
-    h1.answer-title-2.answer-title(v-if="answer_number == 2") L’HOMME QUI RIT
+    h1.question-answer-title-2.question-answer-title(v-if="answer_number == 2") L’HOMME QUI RIT
 
-    h1.answer-title-3.answer-title(v-if="answer_number == 3") DRACULA
+    h1.question-answer-title-3.question-answer-title(v-if="answer_number == 3") DRACULA
 
-    h1.answer-title-4.answer-title(v-if="answer_number == 4") FRANKENSTEIN
+    h1.question-answer-title-4.question-answer-title(v-if="answer_number == 4") FRANKENSTEIN
 
-    h1.answer-title-5.answer-title(v-if="answer_number == 5") Dr JEKYL & Mr HIDE
+    h1.question-answer-title-5.question-answer-title(v-if="answer_number == 5") Dr JEKYLL & Mr HYDE
 
-    h1.answer-title-6.answer-title(v-if="answer_number == 6") LA MOMIE
+    h1.question-answer-title-6.question-answer-title(v-if="answer_number == 6") LA MOMIE
 
-    h1.answer-title-7.answer-title(v-if="answer_number == 7") LE CHAT NOIR
+    h1.question-answer-title-7.question-answer-title(v-if="answer_number == 7") LE CHAT NOIR
 
-    h1.answer-title-8.answer-title(v-if="answer_number == 8") LE CORBEAU
+    h1.question-answer-title-8.question-answer-title(v-if="answer_number == 8") LE CORBEAU
 
-    h1.answer-title-9.answer-title(v-if="answer_number == 9") LE LOUP GAROU
+    h1.question-answer-title-9.question-answer-title(v-if="answer_number == 9") LE LOUP GAROU
 
-    h1.answer-title-10.answer-title(v-if="answer_number == 10") PSYCHOSE
+    h1.question-answer-title-10.question-answer-title(v-if="answer_number == 10") PSYCHOSE
 
 
 
@@ -65,130 +65,49 @@
     router-link.picture(v-if="answer_number == 10")(:to="{name: 'Schedule'}")
       img.picture(src="static/images/Psychose.jpg")
 
-    p.diff.diffusion(v-if="answer_number == 1") Diffusé a 20:00
+    p.diff.diffusion(v-if="answer_number == 1") Diffusé a <span class="bold">20:00</span>
     p.diffusion(v-if="answer_number == 1") Le vendredi 7 juin 2019
     p.diffusion(v-if="answer_number == 1") Au café-cinéma Videodrome-2 à Marseille   
 
-    p.diff.diffusion(v-if="answer_number == 2") Diffusé à 22:00  
+    p.diff.diffusion(v-if="answer_number == 2") Diffusé à <span class="bold">22:00 </span> 
     p.diffusion(v-if="answer_number == 2") Le vendredi 7 juin 2019
     p.diffusion(v-if="answer_number == 2") Au café-cinéma Videodrome-2 à Marseille  
 
-    p.diff.diffusion(v-if="answer_number == 3") Diffusé à 15:30
+    p.diff.diffusion(v-if="answer_number == 3") Diffusé à <span class="bold">15:30</span>
     p.diffusion(v-if="answer_number == 3") Le samedi 8 juin 2019
     p.diffusion(v-if="answer_number == 3") Au café-cinéma Videodrome-2 à Marseille  
 
-    p.diff.diffusion(v-if="answer_number == 4") Diffusé à 17:30
+    p.diff.diffusion(v-if="answer_number == 4") Diffusé à <span class="bold">17:30</span>
     p.diffusion(v-if="answer_number == 4") Le samedi 8 juin 2019
     p.diffusion(v-if="answer_number == 4") Au café-cinéma Videodrome-2 à Marseille  
 
-    p.diff.diffusion(v-if="answer_number == 5") Diffusé à 20:00
+    p.diff.diffusion(v-if="answer_number == 5") Diffusé à 20:00</span>
     p.diffusion(v-if="answer_number == 5") Le samedi 8 juin 2019
     p.diffusion(v-if="answer_number == 5") Au café-cinéma Videodrome-2 à Marseille 
 
-    p.diff.diffusion(v-if="answer_number == 6") Diffusé à 22:30
+    p.diff.diffusion(v-if="answer_number == 6") Diffusé à <span class="bold">22:30</span>
     p.diffusion(v-if="answer_number == 6") Le samedi 8 juin 2019
     p.diffusion(v-if="answer_number == 6") Au café-cinéma Videodrome-2 à Marseille 
 
-    p.diff.diffusion(v-if="answer_number == 7") Diffusé à 15:30
+    p.diff.diffusion(v-if="answer_number == 7") Diffusé à <span class="bold">15:30</span>
     p.diffusion(v-if="answer_number == 7") Le dimanche 9 juin 2019
     p.diffusion(v-if="answer_number == 7") Au café-cinéma Videodrome-2 à Marseille 
 
-    p.diff.diffusion(v-if="answer_number == 8") Diffusé à 17:30
+    p.diff.diffusion(v-if="answer_number == 8") Diffusé à <span class="bold">17:30</span>
     p.diffusion(v-if="answer_number == 8") Le dimanche 9? 2019
     p.diffusion(v-if="answer_number == 8") Au café-cinéma Videodrome-2 à Marseille 
 
-    p.diff.diffusion(v-if="answer_number == 9") Diffusé à 20:00 
+    p.diff.diffusion(v-if="answer_number == 9") Diffusé à <span class="bold">20:00</span>
     p.diffusion(v-if="answer_number == 9") Le dimanche 9 juin 2019 
     p.diffusion(v-if="answer_number == 9") Au café-cinéma Videodrome-2 à Marseille 
 
-    p.diff.diffusion(v-if="answer_number == 10") Diffusé à 22:00 
+    p.diff.diffusion(v-if="answer_number == 10") Diffusé à <span class="bold">22:00</span>
     p.diffusion(v-if="answer_number == 10") Le dimanche 9 juin 2019 
     p.diffusion(v-if="answer_number == 10") Au café-cinéma Videodrome-2 à Marseille 
 </template>
 
 <style lang="sass">
 
-//<BACKGROUND>//
-
-.answer-1
-  background-color: #BB3537
-
-.answer-2
-  background-color: #387078
-
-.answer-3
-  background-color: #2A4031
-
-.answer-4
-  background-color: #323639
-
-.answer-5
-  background-color: #241B2A
-
-.answer-6
-  background-color: #010244
-
-.answer-7
-  background-color: #131313
-
-.answer-8
-  background-color: #382532
-
-.answer-9
-  background-color: #2A397A
-
-.answer-10
-  background-color: #000000
-//</BACKGROUND>//
-
-//<TITRE>//
-.answer-title
-  text-align: center
-  padding-top: 20px
-  font-size: 70px
-
-.answer-title-1
-  color: #F1C53E
-  font-family: "AvenirNextCondensed-HeavyItalic-10"
-
-.answer-title-2
-  color: #EDEAE3
-  font-family: "AvenirNextCondensed-DemiBold-03"
-  text-shadow: 5px 0 50px #FC270E
-
-.answer-title-3
-  color: #E42232
-  font-family: "American-Captain"
-  text-shadow: 0 0 50px #EBD860
-
-.answer-title-4
-  color: #DD481C
-  font-family: "manolete"
-
-.answer-title-5
-  color: #D4C95A
-  font-family: "slimlines"
-
-.answer-title-6
-  color: #FCCC37
-  font-family: "Melting-Letter"
-
-.answer-title-7
-  color: #EBE9CE
-  font-family: "ALYSHEN"
-
-.answer-title-8
-  color: #FAEC27
-  font-family: "DK-Magical-Brush"
-
-.answer-title-9
-  color: #E2C148
-  font-family: "Melting-Letter"
-
-.answer-title-10
-  color: #FBD800
-  font-family: "Compacta"
-//</TITRE>//
 .question-pour-un-mechant
   position: absolute
   left: 34px
@@ -199,6 +118,9 @@
 
 .diff
   padding-top: 50px
+
+.bold
+  font-family: "Montserrat-Bold"
 
 .diffusion
   font-family: "Montserrat-Medium"
@@ -214,6 +136,7 @@
 
 .bonne-reponse
   padding-top: 62px
+  padding-bottom: 50px
   font-family: "Montserrat-Medium"
 
 .fantome
--- src/pages/question.vue
+++ src/pages/question.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  section
+  section.section
     h2.question-pour-un-mechant QUESTION POUR UN MECHANT
     h2.page(v-if="question_number == 1") 01/10
     h2.page(v-if="question_number == 2") 02/10
@@ -12,100 +12,78 @@
     h2.page(v-if="question_number == 9") 09/10
     h2.page(v-if="question_number == 10") 10/10
 
-    h1.question-title-1.question-title(v-if="question_number == 1") QUI DORT DANS UN OPERA POUR PECHO?
+    .question-container
+      h1.question-answer-title-1.question-answer-title(v-if="question_number == 1") QUI DORT DANS UN OPERA POUR PECHO?
 
-    h1.question-title-2.question-title(v-if="question_number == 2") QUI A UN SOURIRE À LA COLGATE ?
+      h1.question-answer-title-2.question-answer-title(v-if="question_number == 2") QUI A UN SOURIRE À LA COLGATE ?
 
-    h1.question-title-3.question-title(v-if="question_number == 3") QUI EST LE PLUS GRAND DE TOUS LES VAMPIRES ?
+      h1.question-answer-title-3.question-answer-title(v-if="question_number == 3") QUI EST LE PLUS GRAND DE TOUS LES VAMPIRES ?
 
-    h1.question-title-4.question-title(v-if="question_number == 4") QUI N'AVAIT PAS D'AMIS ?
+      h1.question-answer-title-4.question-answer-title(v-if="question_number == 4") QUI N'AVAIT PAS D'AMIS ?
 
-    h1.question-title-5.question-title(v-if="question_number == 5") QUI EST CONNU POUR SES DEUX PERSONNALITE ?
+      h1.question-answer-title-5.question-answer-title(v-if="question_number == 5") QUI EST CONNU POUR SES DEUX PERSONNALITE ?
 
-    h1.question-title-6.question-title(v-if="question_number == 6") Qui a toujours ses papiers sur lui ?
+      h1.question-answer-title-6.question-answer-title(v-if="question_number == 6") Qui a toujours ses papiers sur lui ?
 
-    h1.question-title-7.question-title(v-if="question_number == 7") Qui est le chat le plus mÉCHANT ?
+      h1.question-answer-title-7.question-answer-title(v-if="question_number == 7") Qui est le chat le plus mÉCHANT ?
 
-    h1.question-title-8.question-title(v-if="question_number == 8") Qui est une balance ?
+      h1.question-answer-title-8.question-answer-title(v-if="question_number == 8") Qui est une balance ?
 
-    h1.question-title-9.question-title(v-if="question_number == 9") Qui n’est pas alle Chez l’esthEticienne depuis 3 mois ?
+      h1.question-answer-title-9.question-answer-title(v-if="question_number == 9") Qui n’est pas alle Chez l’esthEticienne depuis 3 mois ?
 
-    h1.question-title-10.question-title(v-if="question_number == 10") QUI ADORE REGARDER LES FILLES SOUS LA DOUCHE ?
+      h1.question-answer-title-10.question-answer-title(v-if="question_number == 10") 
+        <span class="relative-container">QUI ADORE <img class="crack1 crack" src="static/images/crack1.svg"> </span>
 
-    router-link.reponse.reponse-1(v-if="question_number == 1" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Les rats d'Opéra
-    router-link.reponse.reponse-1(v-if="question_number == 1" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Le fantôme de l'Opéra
-    router-link.reponse.reponse-1(v-if="question_number == 1" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") La danseuse étoilée
+        <span class="relative-container">REGARDER LES FILLES <img class=" crack3 crack" src="static/images/crack3.svg"><img class=" crack4 crack" src="static/images/crack4.svg"></span> 
 
-    router-link.reponse.reponse-2(v-if="question_number == 2" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") l'Homme qui rit
-    router-link.reponse.reponse-2(v-if="question_number == 2" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Je préfère Signal
-    router-link.reponse.reponse-2(v-if="question_number == 2" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Miley Cirus
+        <span class="relative-container">SOUS LA<img class="crack5 crack" src="static/images/crack5.svg"></span>
 
-    router-link.reponse.reponse-3(v-if="question_number == 3" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Dracula
-    router-link.reponse.reponse-3(v-if="question_number == 3" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Edward Collen
-    router-link.reponse.reponse-3(v-if="question_number == 3" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Le petit vampire
+        <span class="relative-container"> DOUCHE ? <img class="crack2 crack" src="static/images/crack2.svg"></span>
 
-    router-link.reponse.reponse-4(v-if="question_number == 4" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Remi
-    router-link.reponse.reponse-4(v-if="question_number == 4" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Ton ami imaginaire
-    router-link.reponse.reponse-4(v-if="question_number == 4" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Frankenstein
+      .link-container
+        router-link.reponse.reponse-1(v-if="question_number == 1" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Les rats d'Opéra
+        router-link.reponse.reponse-1(v-if="question_number == 1" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Le fantôme de l'Opéra
+        router-link.reponse.reponse-1(v-if="question_number == 1" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") La danseuse étoilée
 
-    router-link.reponse.reponse-5(v-if="question_number == 5" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Dr Jekyl & Mr Hide  
-    router-link.reponse.reponse-5(v-if="question_number == 5" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Dr Jekyl
-    router-link.reponse.reponse-5(v-if="question_number == 5" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Mr Hide
+        router-link.reponse.reponse-2(v-if="question_number == 2" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") l'Homme qui rit
+        router-link.reponse.reponse-2(v-if="question_number == 2" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Je préfère Signal
+        router-link.reponse.reponse-2(v-if="question_number == 2" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Miley Cirus
 
-    router-link.reponse.reponse-6(v-if="question_number == 6" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Un gendarme   
-    router-link.reponse.reponse-6(v-if="question_number == 6" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Ton petit cousin fier d’être majeur
-    router-link.reponse.reponse-6(v-if="question_number == 6" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") La momie
+        router-link.reponse.reponse-3(v-if="question_number == 3" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Dracula
+        router-link.reponse.reponse-3(v-if="question_number == 3" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Edward Collen
+        router-link.reponse.reponse-3(v-if="question_number == 3" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Le petit vampire
 
-    router-link.reponse.reponse-7(v-if="question_number == 7" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Le Chat noir  
-    router-link.reponse.reponse-7(v-if="question_number == 7" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Nyan Cat
-    router-link.reponse.reponse-7(v-if="question_number == 7" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Je donne ma langue au chat
+        router-link.reponse.reponse-4(v-if="question_number == 4" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Remi
+        router-link.reponse.reponse-4(v-if="question_number == 4" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Ton ami imaginaire
+        router-link.reponse.reponse-4(v-if="question_number == 4" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Frankenstein
 
-    router-link.reponse.reponse-8(v-if="question_number == 8" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Je balance pas moi  
-    router-link.reponse.reponse-8(v-if="question_number == 8" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") J’y gagne quoi ?
-    router-link.reponse.reponse-8(v-if="question_number == 8" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Le corbeau
+        router-link.reponse.reponse-5(v-if="question_number == 5" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Dr Jekyll & Mr Hyde  
+        router-link.reponse.reponse-5(v-if="question_number == 5" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Dr Jekyl
+        router-link.reponse.reponse-5(v-if="question_number == 5" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Mr Hide
 
-    router-link.reponse.reponse-9(v-if="question_number == 9" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Mere grand 
-    router-link.reponse.reponse-9(v-if="question_number == 9" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Le loup garou
-    router-link.reponse.reponse-9(v-if="question_number == 9" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Ta copine
+        router-link.reponse.reponse-6(v-if="question_number == 6" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Un gendarme   
+        router-link.reponse.reponse-6(v-if="question_number == 6" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Ton petit cousin fier d’être majeur
+        router-link.reponse.reponse-6(v-if="question_number == 6" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") La momie
 
-    router-link.reponse.reponse-10(v-if="question_number == 10" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Les chats  
-    router-link.reponse.reponse-10(v-if="question_number == 10" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Psychose
-    router-link.reponse.reponse-10(v-if="question_number == 10" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Un voisin Voyeur
+        router-link.reponse.reponse-7(v-if="question_number == 7" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Le Chat noir  
+        router-link.reponse.reponse-7(v-if="question_number == 7" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Nyan Cat
+        router-link.reponse.reponse-7(v-if="question_number == 7" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Je donne ma langue au chat
+
+        router-link.reponse.reponse-8(v-if="question_number == 8" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Je balance pas moi  
+        router-link.reponse.reponse-8(v-if="question_number == 8" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") J’y gagne quoi ?
+        router-link.reponse.reponse-8(v-if="question_number == 8" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Le corbeau
+
+        router-link.reponse.reponse-9(v-if="question_number == 9" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Mere grand 
+        router-link.reponse.reponse-9(v-if="question_number == 9" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Le loup garou
+        router-link.reponse.reponse-9(v-if="question_number == 9" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Ta copine
+
+        router-link.reponse.reponse-10(v-if="question_number == 10" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Les chats  
+        router-link.reponse.reponse-10(v-if="question_number == 10" :to="{path: '/reponse/' + question_number}" @click.native="correctAnswer") Psychose
+        router-link.reponse.reponse-10(v-if="question_number == 10" :to="{path: '/reponse/' + question_number}" @click.native="wrongAnswer") Un voisin Voyeur
 </template>
 
 <style lang="sass">
-//<BACKGROUND>//
 
-.question-1
-  background-color: #C7002F
-
-.question-2
-  background-color: #387078
-
-.question-3
-  background-color: #2A4031
-
-.question-4
-  background-color: #323639
-
-.question-5
-  background-color: #241B2A
-
-.question-6
-  background-color: #010244
-
-.question-7
-  background-color: #131313
-
-.question-8
-  background-color: #382532
-
-.question-9
-  background-color: #2A397A
-
-.question-10
-  background-color: #000000
-//</BACKGROUND>//
 
 .question-pour-un-mechant
   position: absolute
@@ -114,6 +92,32 @@
   color: white
   font-size: 40px
   font-family: "Bloodlust"
+
+
+.relative-container
+  position: relative
+.crack
+  position: absolute
+
+.crack1
+  top: 33%
+  left: 10%
+
+.crack3
+  top: 41%
+  left: 8.75%
+
+.crack4
+  top: 6%
+  left: 45%
+
+.crack5
+  top: 30%
+  left: 25%
+
+.crack2
+  top: 35%
+  left: 42%
 
 .page
   position: absolute
@@ -127,7 +131,7 @@
   font-size: 30px
   font-family: "Montserrat-Bold"
   padding-bottom: 20px
-  display: block
+  display: inline-block
   text-align: center
 
 .reponse-1
@@ -161,56 +165,10 @@
   color: #F02403
 //</REPONSE>//
 
-
-//<TITRE>//
-.question-title
-  text-align: center
-  padding-top: 405px
-  padding-bottom: 50px
-  font-size: 70px
-
-.question-title-1
-  color: #F1C53E
-  font-family: "AvenirNextCondensed-HeavyItalic-10"
-
-.question-title-2
-  color: #EDEAE3
-  font-family: "AvenirNextCondensed-DemiBold-03"
-  text-shadow: 5px 0 50px #FC270E
-
-.question-title-3
-  color: #E42232
-  font-family: "American-Captain"
-  text-shadow: 0 0 50px #EBD860
-
-.question-title-4
-  color: #DD481C
-  font-family: "manolete"
-
-.question-title-5
-  color: #D4C95A
-  font-family: "slimlines"
-
-.question-title-6
-  color: #FCCC37
-  font-family: "Melting-Letter"
-
-.question-title-7
-  color: #EBE9CE
-  font-family: "ALYSHEN"
-
-.question-title-8
-  color: #FAEC27
-  font-family: "DK-Magical-Brush"
-
-.question-title-9
-  color: #E2C148
-  font-family: "Melting-Letter"
-
-.question-title-10
-  color: #FBD800
-  font-family: "Compacta"
-//</TITRE>//
+.link-container
+  flex-direction: column
+  display: flex
+  align-items: center
 </style>
 
 <script>
