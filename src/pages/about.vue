504d81b831b77e98e28e3ca4068b1fc98552ca11
thunderfreeze <yoyot32800@gmail.com>
2019-07-23 12:23:26 +0200

    :sparkles: add a "commencer" btn on about.vue

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  section
+  section.section
     div.about-size
       h1.pour-un-mechant QUESTION POUR UN MECHANT
       h1.videodrome VIDEODROME-2
@@ -7,12 +7,14 @@
       p.place2 Videodrome 2 est avant tout un projet né de la volonté de rencontrer, d'échanger et d'experimenter.Projet porté par la SCOP DCA (Diffusion Cinématographique Alternative) et l'association Solaris en charge de al coordination de programmation, il souhaite offrir un lieu unique et polyvalent dédié a la diffusion cinématographique et la cinéphilie.
       p.place2 Situé au coeur de Marseille au 49 cours Julien, il reunit une salle de cinéma de 49 places accueillant 6 projections hebdomadaires non commerciales, un vidéoclub au catalogue locatif de plus de 5400 films, de la rareté aux classiques en provenance des 4 coins du monde et un bistrot ou café-cinéma
       p.place2 Du 7 au 9 juin prochain, se deroulera au Vidéodrome-2, al premeire édition de week-end de l'horreur. Pour découvrir la programmation et les horaires des films, un mini-quizz est organisé. Répondez correctement!
+      .container-cta
+        p.blood POURMECHANT
+        router-link.commencer(:to="{path: '/question/1'}") COMMENCER
 </template>
 
 <style lang="sass">
 .about-size
   width: 50%
-  margin-left: 25%
 
 .about
   background-color: #000
@@ -27,7 +29,7 @@
     font-size: 1.75rem
 
 .pour-un-mechant
-  padding-top: 100px
+  padding-top: 5rem
   font-size: 80px
   font-family: "bloodlust"
 
