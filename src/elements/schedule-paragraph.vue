e33e3f9e68ddf2c81e8bed8e085b04ab42add809
thunderfreeze <yoyot32800@gmail.com>
2019-07-24 12:13:29 +0200

    :wrench: Move so code to a new page

---
+++ src/elements/schedule-paragraph.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  p.horaire 
+    slot(v-if="pastAnswers.includes(answerNumber)")
+    span(v-else) trouve la bonne reponse
+</template>
+
+<script>
+export default {
+  name: "ScheduleParagraph",
+  props: ["answer-number"],
+  computed: {
+    pastAnswers() {
+      return this.$store.getters.pastAnswers;
+    }
+  }
+};
+</script>
+
+<style lang="sass">
+.horaire
+  text-align: center
+  width: 100%
+  font-family: "Montserrat-Medium"
+  font-size: 18px
+  line-height: 30px
+
+  span
+    font-family: "BloodLust"
+    color: #D10003
+    font-size: 28px
+</style>
