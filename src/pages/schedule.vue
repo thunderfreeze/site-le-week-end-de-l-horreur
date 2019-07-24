a89d42a00b3cd934f40aeed12986e7a8279da954
thunderfreeze <yoyot32800@gmail.com>
2019-07-24 12:10:59 +0200

    :sparkles: Change p.horaire to ScheduleParagraph(:answer-number="1,2,3,4,5,6,7,8,9,10"

--- src/pages/schedule.vue
+++ src/pages/schedule.vue
@@ -5,18 +5,18 @@
       h3.prog BRAVO TU AS TERMINE LE QUIZZ
       h3.prog.programation PROGRAMMATION
       p.horaire.date Vendredi 7 juin 2019
-      p.horaire 20:00  Le fantôme de l'opéra (Rupert Julian) 1925
-      p.horaire 22:00  L'homme qui rit (Jonathan Demme) 1928
+      SheduleParagraph(:answer-number="1") 20:00  Le fantôme de l'opéra (Rupert Julian) 1925
+      SheduleParagraph(:answer-number="2") 22:00  L'homme qui rit (Jonathan Demme) 1928
       p.horaire.date Samedi 8 juin 2019
-      p.horaire 15:30  Dracula (Tod Browning) 1931
-      p.horaire 17:30  Frankenstein (James Whale) 1931
-      p.horaire 20:00  Docteur Jekyll et Mister Hyde (Rouben Mamoulian) 1931
-      p.horaire 22:30  La momie (Karl Freund) 1932
+      SheduleParagraph(:answer-number="3") 15:30  Dracula (Tod Browning) 1931
+      SheduleParagraph(:answer-number="4") 17:30  Frankenstein (James Whale) 1931
+      SheduleParagraph(:answer-number="5") 20:00  Docteur Jekyll et Mister Hyde (Rouben Mamoulian) 1931
+      SheduleParagraph(:answer-number="6") 22:30  La momie (Karl Freund) 1932
       p.horaire.date  Dimanche 9 juin 2019
-      p.horaire 15:30  Le chat noir (Edgar G. Ulmer) 1934
-      p.horaire 17:30  Le corbeau (Lew Landers) 1935
-      p.horaire 20:00  Le loup garou (M.?ight Shyamalan)1941
-      p.horaire 22:00  Psychose (Alphred Hitchcock)1960
+      SheduleParagraph(:answer-number="7") 15:30  Le chat noir (Edgar G. Ulmer) 1934
+      SheduleParagraph(:answer-number="8") 17:30  Le corbeau (Lew Landers) 1935
+      SheduleParagraph(:answer-number="9") 20:00  Le loup garou (M.?ight Shyamalan)1941
+      SheduleParagraph(:answer-number="10") 22:00  Psychose (Alphred Hitchcock)1960
       p.adresse.pad AU CINEMA-CAFE VIDEODROME-2
       p.adresse 49 Cours Julien - 13006 Marseille
       p.adresse 04 91 42 75 41
@@ -74,12 +74,6 @@
   padding-bottom: 26px
   font-family: "Bloodlust"
 
-.horaire
-  text-align: center
-  width: 100%
-  font-family: "Montserrat-Medium"
-  font-size: 18px
-
 .date
   padding-top: 40px
   font-family: "Montserrat-Bold"
@@ -94,8 +88,13 @@
 </style>
 
 <script>
+import SheduleParagraph from "@/elements/schedule-paragraph";
+
 export default {
   name: "Schedule",
+  components: {
+    SheduleParagraph
+  },
   methods: {
     resume() {
       this.$router.go(-1);
